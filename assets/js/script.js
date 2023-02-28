f0a3d790cbda7023009e8482d23a810a46b1a2f7
CGoddard1 <goddard.cole@gmail.com>
2023-02-28 18:01:25 -0500

    corrected js and removed comments in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,13 @@ $(function () {
     localStorage.setItem(timeBlockId, userInput);
   });
 //local storage save for elements in the time block
-  $('.time-block').each(function () {
+  $('.time-block').each(function() {
     var timeBlockId = $(this).attr('id');
     var userInput = localStorage.getItem(timeBlockId);
     $(this).find('.description').val(userInput);
 //sets current hour and timeblock variables
     var currentHour = dayjs().hour();
-    var timeBlockHour = parseInt($(this).attr('id').split('_')[1]);
+    var timeBlockHour = parseInt($(this).attr('id').split('-')[1]);
 //tells whether it is the past present or future and adds a class in conjunction with the time
     if (timeBlockHour < currentHour) {
       $(this).addClass('past');
@@ -23,6 +23,8 @@ $(function () {
     }
   })
 //displays current date and time at the top of the page
+var timeInterval = setInterval(function () {
   var today = dayjs();
   $('#currentDay').text(today.format('MMMM D, YYYY, h:mm:ss'));
 }, 1000);
+});
